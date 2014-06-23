8d49e4dff3471a180b61ae081c42a02b492930b5
Fahad Mukunzi <fahadm507@gmail.com>
2014-06-23 08:15:31 -0400

    Add more validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, presence: true
-  validates :city, presence: true, length: { maximum: 5 }
-  validates :zip_code, presence: true
+  validates :city, presence: true, length: {maximum:30}
+  validates :zip_code, presence: true, length: {maximum:5}
 end
